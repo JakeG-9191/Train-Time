afee058c5a5a2711c0913bc4d7f010d89d48bbe4
Jake Garlick <jacobgarlick@msn.com>
2019-09-21 14:13:09 -0600

    added table, linked with firebase and bootstrap

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDD6FIZt8cSJqZgHXKq63niLgXp5R2GJ2k",
+    authDomain: "test-718af.firebaseapp.com",
+    databaseURL: "https://test-718af.firebaseio.com",
+    projectId: "test-718af",
+    storageBucket: "",
+    messagingSenderId: "1095688204984",
+    appId: "1:1095688204984:web:e6da693ac3a05974b7ba4d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
