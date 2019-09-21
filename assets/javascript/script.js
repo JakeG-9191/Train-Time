a529274836747d939eba38279913b48a91df75b7
Jake Garlick <jacobgarlick@msn.com>
2019-09-21 14:43:12 -0600

    built out user input form in HTML and began to build jquery to grab data entry for storage in firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,30 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDD6FIZt8cSJqZgHXKq63niLgXp5R2GJ2k",
-    authDomain: "test-718af.firebaseapp.com",
-    databaseURL: "https://test-718af.firebaseio.com",
-    projectId: "test-718af",
+    apiKey: "AIzaSyDTISWFYC4ZGeu-F2dHRc6pSPVl5NWFk6A",
+    authDomain: "test-c3c9e.firebaseapp.com",
+    databaseURL: "https://test-c3c9e.firebaseio.com",
+    projectId: "test-c3c9e",
     storageBucket: "",
-    messagingSenderId: "1095688204984",
-    appId: "1:1095688204984:web:e6da693ac3a05974b7ba4d"
-};
+    messagingSenderId: "373825459098",
+    appId: "1:373825459098:web:baeeefca2835bdcc542023"
+  };
 
 firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name").val().trim();
+    var trainDest = $("#train-destination").val().trim();
+    var trainStart = $("#train-first").val().trim();
+    var trainRate = $("#train-rate").val().trim();
+    console.log(trainName)
+
+    database.ref().push({
+        trainName,
+        trainDest,
+        trainStart,
+        trainRate,
+    });
+});
