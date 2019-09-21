260c5362f79ad055833cf882661fc7127e3fb432
Jake Garlick <jacobgarlick@msn.com>
2019-09-21 15:33:23 -0600

    full integration with firebase, return results on user click event, adding function for time calculation with moment.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -27,4 +27,34 @@ $("#submit").on("click", function (event) {
         trainStart,
         trainRate,
     });
+    timeCreation();
 });
+
+database.ref().on("child_added", function (childSnapshot) {
+    var dateFormat = "YYYY/MM/DD";
+    
+    console.log(timeConverted)
+    
+    var childName = (childSnapshot.val().trainName);
+    var childDest = (childSnapshot.val().trainDest);
+    var childRate = (childSnapshot.val().trainRate);
+    var childNext = "Hold";
+    var childAway = "Hold";
+    
+    $(".info-added").append(`<tr><td
+    id="table-name">${childName}</td><td
+    id="table-destination">${childDest}</td><td
+    id="table-rate">${childRate}</td><td
+    id="table-next">${childNext}</td><td
+    id="table-away">${childAway}</td></tr>`);
+    
+}), function (errorObject) {
+    console.log("This has failed: " + errorObject.code);
+};
+
+function timeCreation (){
+    var timeConverted = moment(trainStart, "HH:mm").subtract(1, "day");
+    console.log(timeConverted)
+    var currentTime = moment();
+
+}
